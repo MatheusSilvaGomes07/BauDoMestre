d479433c581189be68d0ee1ac97abd6ade0a1522
MatheusSilvaGomes <matheus.mape@gmail.com>
2023-06-06 15:03:21 -0300

    15:02 // 06/06/2023 // Cache
    Nem sei oq mudou, mas tá pedindo e provavelmente deve ser cache.

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('login', views.login, name= 'login'),
     path('cadastro', views.cadastro, name = 'cadastro'),
-    path('mural', views.mural, name = 'mural')
+    path('mural', views.mural, name = 'mural'),
 ]
