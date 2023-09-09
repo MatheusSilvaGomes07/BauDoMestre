1c3b01fc2f72feccaf036d1542585fd5a75c7ae9
Lucas Passos da Silva <LucasPassosSilva2207@gmail.com>
2023-09-09 18:25:51 -0300

    18:25 // 09/09/2023 // Finalizando estrutura da ficha de Ordem Paranormal
    Terminada a ficha de Ordem Paranormal com algumas gambiarras em Ataques e Habilidades. Ainda sem o front-end e sem nenhum JS

--- meus_personagens/models.py
+++ meus_personagens/models.py
@@ -137,6 +137,9 @@ class DnD(Personagem):
     caracteristicas = models.TextField(null=True, blank=True)
 
 
+
+
+
 class OrdemParanormal(Personagem):
 
     #Atributos
@@ -146,7 +149,7 @@ class OrdemParanormal(Personagem):
     pre = models.IntegerField(default=0)
     forc = models.IntegerField(default=0)
     
-
+    #Outros dados
     origem = models.CharField(max_length=100)
 
     classesOptions = [
@@ -172,4 +175,173 @@ class OrdemParanormal(Personagem):
     resistencias = models.CharField(max_length=250, blank=True, null=True)
 
 
-   
+    #Pericias
+    periciasNivel = [
+            ('Leigo', 'Leigo'),
+            ('Treinado', 'Treinado'),
+            ('Veterano', 'Veterano'),
+            ('Especialista', 'Especialista'),
+    ]
+
+    acrobacia = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    acrobaciaOutros = models.IntegerField(default=0)
+
+    adestramento = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    adestramentoOutros = models.IntegerField(default=0)
+
+    artes = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    artesOutros = models.IntegerField(default=0)
+
+    atletismo = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    atletismoOutros = models.IntegerField(default=0)
+
+    atualidades = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    atualidadesOutros = models.IntegerField(default=0)
+
+    ciencias = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    cienciasOutros = models.IntegerField(default=0)
+
+    crime = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    crimeOutros = models.IntegerField(default=0)
+
+    diplomacia = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    diplomaciaOutros = models.IntegerField(default=0)
+
+    enganacao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    enganacaoOutros = models.IntegerField(default=0)
+
+    fortitude = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    fortitudeOutros = models.IntegerField(default=0)
+
+    furtividade = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    furtividadeOutros = models.IntegerField(default=0)
+
+    iniciativa = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    iniciativaOutros = models.IntegerField(default=0)
+
+    intimidacao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    intimidacaoOutros = models.IntegerField(default=0)
+
+    intuicao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    intuicaoOutros = models.IntegerField(default=0)
+
+    investigacao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    investigacaoOutros = models.IntegerField(default=0)
+
+    luta = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    lutaOutros = models.IntegerField(default=0)
+
+    medicina = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    medicinaOutros = models.IntegerField(default=0)
+
+    ocultismo = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    ocultismoOutros = models.IntegerField(default=0)
+
+    percepcao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    percepcaoOutros = models.IntegerField(default=0)
+
+    pilotagem = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    pilotagemOutros = models.IntegerField(default=0)
+
+    pontaria = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    pontariaOutros = models.IntegerField(default=0)
+
+    profissao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    profissaoOutros = models.IntegerField(default=0)
+
+    reflexos = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    reflexosOutros = models.IntegerField(default=0)
+
+    religiao = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    religiaoOutros = models.IntegerField(default=0)
+
+    sobrevivencia = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    sobrevivenciaOutros = models.IntegerField(default=0)
+
+    tatica = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    taticaOutros = models.IntegerField(default=0)
+
+    tecnologia = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    tecnologiaOutros = models.IntegerField(default=0)
+
+    vontade = models.CharField(max_length=20, choices=periciasNivel, default='Leigo')
+    vontadeOutros = models.IntegerField(default=0)
+
+   #Ataques
+    ataqueNome1 = models.CharField(max_length=150, blank=True, null=True)
+    tipo1 = models.CharField(max_length=50, blank=True, null=True)
+    teste1 = models.CharField(max_length=50, blank=True, null=True)
+    dano1 = models.CharField(max_length=50, blank=True, null=True)
+    critico1 = models.CharField(max_length=50, blank=True, null=True)
+    alcance1 = models.CharField(max_length=100, blank=True, null=True)
+    especial1 = models.CharField(max_length=50, blank=True, null=True)
+
+    ataqueNome2 = models.CharField(max_length=150, blank=True, null=True)
+    tipo2 = models.CharField(max_length=50, blank=True, null=True)
+    teste2 = models.CharField(max_length=50, blank=True, null=True)
+    dano2 = models.CharField(max_length=50, blank=True, null=True)
+    critico2 = models.CharField(max_length=50, blank=True, null=True)
+    alcance2 = models.CharField(max_length=100, blank=True, null=True)
+    especial2 = models.CharField(max_length=50, blank=True, null=True)
+
+    ataqueNome3 = models.CharField(max_length=150, blank=True, null=True)
+    tipo3 = models.CharField(max_length=50, blank=True, null=True)
+    teste3 = models.CharField(max_length=50, blank=True, null=True)
+    dano3 = models.CharField(max_length=50, blank=True, null=True)
+    critico3 = models.CharField(max_length=50, blank=True, null=True)
+    alcance3 = models.CharField(max_length=100, blank=True, null=True)
+    especial3 = models.CharField(max_length=50, blank=True, null=True)
+
+    ataqueNome4 = models.CharField(max_length=150, blank=True, null=True)
+    tipo4 = models.CharField(max_length=50, blank=True, null=True)
+    teste4 = models.CharField(max_length=50, blank=True, null=True)
+    dano4 = models.CharField(max_length=50, blank=True, null=True)
+    critico4 = models.CharField(max_length=50, blank=True, null=True)
+    alcance4 = models.CharField(max_length=100, blank=True, null=True)
+    especial4 = models.CharField(max_length=50, blank=True, null=True)
+
+    ataqueNome5 = models.CharField(max_length=150, blank=True, null=True)
+    tipo5 = models.CharField(max_length=50, blank=True, null=True)
+    teste5 = models.CharField(max_length=50, blank=True, null=True)
+    dano5 = models.CharField(max_length=50, blank=True, null=True)
+    critico5 = models.CharField(max_length=50, blank=True, null=True)
+    alcance5 = models.CharField(max_length=100, blank=True, null=True)
+    especial5 = models.CharField(max_length=50, blank=True, null=True)
+
+    ataqueNome6 = models.CharField(max_length=150, blank=True, null=True)
+    tipo6 = models.CharField(max_length=50, blank=True, null=True)
+    teste6 = models.CharField(max_length=50, blank=True, null=True)
+    dano6 = models.CharField(max_length=50, blank=True, null=True)
+    critico6 = models.CharField(max_length=50, blank=True, null=True)
+    alcance6 = models.CharField(max_length=100, blank=True, null=True)
+    especial6 = models.CharField(max_length=50, blank=True, null=True)
+
+
+    #Habilidades e rituais
+    dt = models.IntegerField(default=0)
+    habilidades = models.TextField(null=True, blank=True)
+
+
+    #Inventário
+    pp = models.IntegerField(default=0)
+    patente = models.CharField(max_length=100, null=True, blank=True)
+    #Limite de itens
+    carga1 = models.BooleanField(default=False)
+    carga2 = models.BooleanField(default=False)
+    carga3 = models.BooleanField(default=False)
+    carga4 = models.BooleanField(default=False)
+
+    limiteCredito = models.CharField(max_length=50, null=True, blank=True)
+
+    cargaAtual = models.IntegerField(default=0)
+    cargaMax = models.IntegerField(default=0)
+
+
+    itens = models.TextField(null=True, blank=True)
+
+
+    #Descrição
+    aparencia = models.CharField(max_length=500, null=True, blank=True)
+    personalidade = models.CharField(max_length=500, null=True, blank=True)
+    historico = models.CharField(max_length=500, null=True, blank=True)
+    objetivo = models.CharField(max_length=500, null=True, blank=True)
