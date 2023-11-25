7d46941f2ca10ba1c57472667634ab90a9bac45c
MatheusSilvaGomes <matheus.mape@gmail.com>
2023-11-25 19:43:48 -0300

    19:43 // 25/11/2023 // Teste de deploy Alteração no requirements.txt, Dockerfile e settings.py

--- BauDoMestre/settings.py
+++ BauDoMestre/settings.py
@@ -78,19 +78,6 @@ MIDDLEWARE = [
     'inventario.middleware.MaliciousFileCheckMiddleware',
 ]
 
-if DEBUG == True:
-    MIDDLEWARE = [
-        'allauth.account.middleware.AccountMiddleware',
-        'django.middleware.security.SecurityMiddleware',
-        'whitenoise.middleware.WhiteNoiseMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'inventario.middleware.MaliciousFileCheckMiddleware',
-    ]
 
 ROOT_URLCONF = 'BauDoMestre.urls'
 
