8b74a15ddfc7040be6d7f6a1571b47b9a914da4a
Lucas Passos da Silva <LucasPassosSilva2207@gmail.com>
2024-07-11 00:53:48 -0300

    00:53 // 11/07/2024 // Decorator de login_required
    Implementação simples do decorator para impedir que usuários não logados possam acessar a campanha

--- tabletop/views.py
+++ tabletop/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Map, Token
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def enter_campaign(request, campaign_id):
     maps = Map.objects.filter(campanha_id=campaign_id)
     return render(request, 'tabletop/campaign.html', {'maps': maps, 'campaign_id': campaign_id})
 
+@login_required
 def load_map(request, map_id):
     current_map = get_object_or_404(Map, id=map_id)
     tokens = Token.objects.filter(map=current_map)
     return render(request, 'tabletop/map.html', {'map': current_map, 'tokens': tokens})
 
+@login_required 
 @csrf_exempt
 def move_token(request, token_id):
     if request.method == 'POST':
